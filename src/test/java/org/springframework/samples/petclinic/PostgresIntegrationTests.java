d32e462a4ee0912293f66f2f9006ae1253c6543f
Nayan121com <59618832+Nayan121com@users.noreply.github.com>
2023-08-28 12:35:50 +0530

    Update PostgresIntegrationTests.java

--- src/test/java/org/springframework/samples/petclinic/PostgresIntegrationTests.java
+++ src/test/java/org/springframework/samples/petclinic/PostgresIntegrationTests.java
@@ -1,81 +1,81 @@
-// /*
-//  * Copyright 2012-2019 the original author or authors.
-//  *
-//  * Licensed under the Apache License, Version 2.0 (the "License");
-//  * you may not use this file except in compliance with the License.
-//  * You may obtain a copy of the License at
-//  *
-//  *      https://www.apache.org/licenses/LICENSE-2.0
-//  *
-//  * Unless required by applicable law or agreed to in writing, software
-//  * distributed under the License is distributed on an "AS IS" BASIS,
-//  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-//  * See the License for the specific language governing permissions and
-//  * limitations under the License.
-//  */
+/*
+ * Copyright 2012-2019 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
-// package org.springframework.samples.petclinic;
+package org.springframework.samples.petclinic;
 
-// import static org.assertj.core.api.Assertions.assertThat;
-// import static org.junit.jupiter.api.Assumptions.assumeTrue;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assumptions.assumeTrue;
 
-// import java.util.Arrays;
-// import java.util.LinkedList;
-// import java.util.List;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
 
-// import org.apache.commons.logging.Log;
-// import org.apache.commons.logging.LogFactory;
-// import org.junit.jupiter.api.BeforeAll;
-// import org.junit.jupiter.api.Test;
-// import org.junit.jupiter.api.condition.DisabledInNativeImage;
-// import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.boot.builder.SpringApplicationBuilder;
-// import org.springframework.boot.context.event.ApplicationPreparedEvent;
-// import org.springframework.boot.test.context.SpringBootTest;
-// import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
-// import org.springframework.boot.test.web.server.LocalServerPort;
-// import org.springframework.boot.web.client.RestTemplateBuilder;
-// import org.springframework.context.ApplicationListener;
-// import org.springframework.core.env.ConfigurableEnvironment;
-// import org.springframework.core.env.EnumerablePropertySource;
-// import org.springframework.core.env.PropertySource;
-// import org.springframework.http.HttpStatus;
-// import org.springframework.http.RequestEntity;
-// import org.springframework.http.ResponseEntity;
-// import org.springframework.samples.petclinic.vet.VetRepository;
-// import org.springframework.test.context.ActiveProfiles;
-// import org.springframework.web.client.RestTemplate;
-// import org.testcontainers.DockerClientFactory;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.DisabledInNativeImage;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.event.ApplicationPreparedEvent;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.ApplicationListener;
+import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.core.env.EnumerablePropertySource;
+import org.springframework.core.env.PropertySource;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.samples.petclinic.vet.VetRepository;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.web.client.RestTemplate;
+import org.testcontainers.DockerClientFactory;
 
-// @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT, properties = { "spring.docker.compose.skip.in-tests=false", //
-// 		"spring.docker.compose.profiles.active=postgres" })
-// @ActiveProfiles("postgres")
-// @DisabledInNativeImage
-// public class PostgresIntegrationTests {
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT, properties = { "spring.docker.compose.skip.in-tests=false", //
+		"spring.docker.compose.profiles.active=postgres" })
+@ActiveProfiles("postgres")
+@DisabledInNativeImage
+public class PostgresIntegrationTests {
 
-// 	@LocalServerPort
-// 	int port;
+	@LocalServerPort
+	int port;
 
-// 	@Autowired
-// 	private VetRepository vets;
+	@Autowired
+	private VetRepository vets;
 
-// 	@Autowired
-// 	private RestTemplateBuilder builder;
+	@Autowired
+	private RestTemplateBuilder builder;
 
-// 	@BeforeAll
-// 	static void available() {
-// 		assumeTrue(DockerClientFactory.instance().isDockerAvailable(), "Docker not available");
-// 	}
+	@BeforeAll
+	static void available() {
+		assumeTrue(DockerClientFactory.instance().isDockerAvailable(), "Docker not available");
+	}
 
-// 	public static void main(String[] args) {
-// 		new SpringApplicationBuilder(PetClinicApplication.class) //
-// 			.profiles("postgres") //
-// 			.properties( //
-// 					"spring.docker.compose.profiles.active=postgres" //
-// 			) //
-// 			.listeners(new PropertiesLogger()) //
-// 			.run(args);
-// 	}
+	public static void main(String[] args) {
+		new SpringApplicationBuilder(PetClinicApplication.class) //
+			.profiles("postgres") //
+			.properties( //
+					"spring.docker.compose.profiles.active=postgres" //
+			) //
+			.listeners(new PropertiesLogger()) //
+			.run(args);
+	}
 
 // 	@Test
 // 	void testFindAll() throws Exception {
